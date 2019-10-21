9d61a58e6c02bedc39c209ec939ecd5c42f24a92
ThomasNGit <neronithomas1989@gmail.com>
2019-10-21 14:53:22 +0200

    problema a stampare a schermo se l'usermail è presente nell'array

--- mail/assets/javascript/main.js
+++ mail/assets/javascript/main.js
@@ -3,27 +3,24 @@
 // prompt per chiedere all'utente il suo indirizzo email
 var usermail = prompt("Qual è il tuo indirizzo email?")
 
-console.log(usermail);
 
 
 // creazione array con lista email degli invitati
-var invitati = ["mail1", "mail2", "mail3"]
+var invitati = ["mail1", "mail2", "mail3"];
 
-console.log(invitati);
 
 
 // creazione del ciclo per stabilire se la mail è presente nella lista degli invitati
 for (var i = 0; i < invitati.length; i++) {
 
-  if (usermail != invitati[i]){
+  if (invitati[i] == usermail) {
 
-  document.getElementById('mioid').innerHTML = "non sei invitato";
+    document.getElementById("mioid").innerHTML = "Sì";
 
+  } else {
 
-} else{
+    document.getElementById("mioid").innerHTML = "No";
 
-  document.getElementById('mioid').innerHTML = "sei invitato";
-
-}
+  }
 
 }
