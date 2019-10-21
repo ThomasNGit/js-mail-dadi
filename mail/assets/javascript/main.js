60a7f5563c3e61df3744bec52cf9864ed929e9c1
ThomasNGit <neronithomas1989@gmail.com>
2019-10-21 16:17:25 +0200

    Dichiarate le variabili a capo codice

--- mail/assets/javascript/main.js
+++ mail/assets/javascript/main.js
@@ -1,15 +1,19 @@
 // Chiedi all’utente la sua email controlla che sia nella lista di chi può accedere e stampa un messaggio appropriato;
 
+
+// dichiarazione delle variabili
+var nome, usermail, invitati, invitato;
+
 // prompt che chiede il nome all'utente
-var nome = prompt("Qual è il tuo nome?")
+nome = prompt("Qual è il tuo nome?")
 
 // prompt per chiedere all'utente il suo indirizzo email
-var usermail = prompt("Qual è il tuo indirizzo email?")
+usermail = prompt("Qual è il tuo indirizzo email?")
 
 
 
 // creazione array con lista email degli invitati
-var invitati = ["mail1", "mail2", "mail3"];
+invitati = ["mail1", "mail2", "mail3"];
 
 // variabile appoggio per vedere se sei stato invitato
 invitato = false
